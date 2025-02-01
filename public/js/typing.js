bcd638281220057ca4ec16ed54a6471bb20693a0
Wlad <136496464+VladTer06081963@users.noreply.github.com>
2025-02-01 21:09:05 +0200

    fixed module news and add finanse

--- public/js/typing.js
+++ public/js/typing.js
@@ -28,28 +28,3 @@ document.addEventListener("DOMContentLoaded", () => {
   typeEffect();
 });
 
-// /** @format */
-
-// // typing.js
-// document.addEventListener("DOMContentLoaded", () => {
-//   const textElement = document.getElementById("typing-text");
-//   const text = "Erfolgreiche Marken entstehen durch engagierte Menschen";
-//   // let text =  text_ser ;
-//   let index = 0;
-
-//   function typeEffect() {
-//     if (index < text.length) {
-//       textElement.textContent += text[index];
-//       index++;
-//       setTimeout(typeEffect, 150);
-//     } else {
-//       setTimeout(() => {
-//         textElement.textContent = "";
-//         index = 0;
-//         typeEffect();
-//       }, 5000);
-//     }
-//   }
-
-//   typeEffect();
-// });
